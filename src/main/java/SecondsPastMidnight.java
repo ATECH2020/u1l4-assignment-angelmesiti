fcbfe85e8ac0d2ebd1e957acd6d85e9649814e5b
Angel Mesiti <angel.233622@nv.ccsd.net>
2020-09-07 01:44:33 +0000

    I added the finishing code

--- src/main/java/SecondsPastMidnight.java
+++ src/main/java/SecondsPastMidnight.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
-
+ 
 class SecondsPastMidnight {
-    public static void main(String[] args) {
-
-        //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
-
-        //Reads int from user
-        int number = scanner.nextInt();
-
-        //Your code goes here
-
-
-        // closing the scanner object
-        scanner.close();
-    }
+   public static void main(String[] args) {
+ 
+       //Creates a new scanner for user input
+       Scanner scanner = new Scanner(System.in);
+ 
+       //Reads int from user
+       int number = scanner.nextInt();
+       int hours = number / 3600;
+       int minutes = number / 60;
+       System.out.print(hours + " " + minutes);
+ 
+       //Your code goes here
+ 
+ 
+       // closing the scanner object
+       scanner.close();
+   }
 }
--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -1,20 +1,42 @@
 import java.util.Scanner;
-
+ 
 class StudentDesks {
-    public static void main(String[] args) {
-
-        //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
-
-        //Reads ints from user
-        
-
-        /*
-         *  your code goes here
-         */
-
-
-        // closing the scanner object
-        scanner.close();
-    }
+   public static void main(String[] args) {
+ 
+       //Creates a new scanner for user input
+       Scanner scanner = new Scanner(System.in);
+ 
+       //reads int from user
+       int a = scanner.nextInt();
+       int b = scanner.nextInt();
+       int c = scanner.nextInt();
+      
+       int classA;
+       int classB;
+       int classC;
+ 
+             /*
+           *  your code goes here
+           */
+           if (a % 2 == 1){
+             a = a + 1;
+           }
+           if (b % 2 == 1){
+             b = b + 1;
+           }
+           if (c % 2 == 1){
+             c = c + 1;
+           }
+           classA = a/2;
+           classB = b/2;
+           classC = c/2;
+ 
+ 
+           System.out.println("classA " + classA);
+           System.out.println("classB " + classB);
+           System.out.println("classC " + classC);
+           System.out.print(classA + classB + classC);
+         // closing the scanner object
+       scanner.close();
+   }
 }
